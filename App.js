6d5f7fb4f94db99ffeda42224b107e353f574263
Jason Zhang <83345246+juesenggg@users.noreply.github.com>
2024-05-01 17:36:19 +0930

    Add files via upload

---
+++ App.js
@@ -0,0 +1,237 @@
+import React, { Component } from 'react'
+import {
+  StatusBar,
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native'
+
+import Card from './Card'
+
+class App extends Component {
+  state = {
+    cardSymbols: [
+      '💩', '🤡', '🎃', '😈', '🤖', ' 🦷', '🌕', '🔥', '🍄', '⚽️', '🎁', '🧧'
+    ],
+    cardSymbolsInRand: [],
+    isOpen: [],
+    firstPickedIndex: null,
+    secondPickedIndex: null,
+    steps: 0,
+    isEnded: false,
+  }
+
+  initGame = () => {
+    let newCardsSymbols = [...this.state.cardSymbols, ...this.state.cardSymbols]
+    let cardSymbolsInRand = this.shuffleArray(newCardsSymbols)
+    
+    let isOpen = []
+    for (let i = 0; i < newCardsSymbols.length; i++){
+      isOpen.push(false)
+    }
+
+    this.setState({
+      cardSymbolsInRand,
+      isOpen,
+    })
+  }
+
+  componentDidMount() {
+    this.initGame()
+  }
+
+  shuffleArray = (arr) => {
+    const newArr = arr.slice()
+    for (let i = newArr.length - 1; i > 0; i--){
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]]
+    }
+    return newArr
+  }
+
+  cardPressHandler = (index) => {
+    let isOpen = [...this.state.isOpen]
+
+    if (isOpen[index]){
+      return;
+    }
+
+    isOpen[index] = true
+
+    if (this.state.firstPickedIndex == null && this.setState.secondPickedIndex == null){
+      this.setState({
+        isOpen,
+        firstPickedIndex: index,
+      })
+    }else if (this.state.firstPickedIndex != null && this.state.secondPickedIndex == null){
+      this.setState({
+        isOpen,
+        secondPickedIndex: index,
+      })
+    }
+
+    this.setState({
+      steps: this.state.steps + 1,
+    })
+  }
+
+
+
+  calculateGameResult = () => {
+    if (this.state.firstPickedIndex != null && this.state.secondPickedIndex != null){
+      
+      if (this.state.cardSymbolsInRand.length > 0){
+        let totalOpens = this.state.isOpen.filter((isOpen) => isOpen)
+        if (totalOpens.length === this.state.cardSymbolsInRand.length){
+          this.setState({
+            isEnded: true,
+          })
+          return
+        }
+      }
+
+      let firstSymbol = this.state.cardSymbolsInRand[this.state.firstPickedIndex]
+      let secondSymbol = this.state.cardSymbolsInRand[this.state.secondPickedIndex]
+
+      if(firstSymbol != secondSymbol){
+
+        setTimeout(() => {
+          let isOpen = [...this.state.isOpen]
+          isOpen[this.state.firstPickedIndex] = false
+          isOpen[this.state.secondPickedIndex] = false
+
+          this.setState({
+            firstPickedIndex: null,
+            secondPickedIndex: null,
+            isOpen,
+          })
+
+
+        }, 1000)
+      }else{
+        this.setState({
+          firstPickedIndex: null,
+          secondPickedIndex: null,
+        })      
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.secondPickedIndex != this.state.secondPickedIndex) {
+      this.calculateGameResult()
+
+    }
+  }
+
+  resetGame = () => {
+    this.initGame()
+
+    this.setState({
+      firstPickedIndex: null,
+      secondPickedIndex: null,
+      steps: 0,
+      isEnded: false,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar />
+        <SafeAreaView style={ styles.container }>
+          <View style={ styles.header}>
+            <Text style={ styles.heading }>Matching Game</Text>
+          </View>
+
+          <View style={ styles.main }>
+            <View style={ styles.gameBoard }>
+              {this.state.cardSymbolsInRand.map((symbol, index) =>
+              <Card key={index} onPress={ () => this.cardPressHandler(index) } style={ styles.button } fontSize={22} title={symbol} cover="⁇" isShow={this.state.isOpen[index]} />
+              )}
+            </View>
+          </View>
+
+          <View style= { styles.footer }>
+            <Text style={ styles.footerText }>{
+              this.state.isEnded 
+              ? `Congrats! You have completed in ${this.state.steps} steps.`
+              : `You have tried ${this.state.steps} time(s).`
+            }</Text>
+            {this.state.isEnded ?
+              <TouchableOpacity onPress={ this.resetGame } style= { styles.tryAgainButton }>
+               <Text style={ styles.tryAgainButtonText }>Try Again</Text>
+              </TouchableOpacity>
+              : null}
+          </View>
+        </SafeAreaView>
+
+      </>
+    )
+  }
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  header:{
+    flex:1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading:{
+      fontSize:32,
+      fontWeight: 'bold',
+      textAlign: 'center',
+  },
+  main:{
+    flex:5,
+    backgroundColor: '#fff',
+  },
+  footer:{
+    flex:1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footerText:{
+    fontSize:18,
+    textAlign: 'center',
+  },
+  gameBoard:{
+    flex:1,
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  button:{
+    backgroundColor:'#eee',
+    borderRadius: 8,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin:(Dimensions.get('window').width - (30 * 6)) / (7 * 2),
+  },
+  tryAgainButton:{
+    backgroundColor:'#eee',
+    padding: 8,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  tryAgainButtonText:{
+    fontSize: 16,
+  },
+  buttonText:{
+    fontSize: 22,
+  },
+}) 
